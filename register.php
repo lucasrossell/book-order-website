af5294de87116e27a018b8724e8cedef50f3a387
Luis Vargas <32406288+lvarg046@users.noreply.github.com>
2021-11-29 22:02:12 -0500

    Completed front end and added (maybe?) working php functionality to text fields.

--- register.php
+++ register.php
@@ -4,14 +4,14 @@
 
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
- 
+$username = $password = $confirm_password = $userEmail = $fullName = "";
+$username_err = $password_err = $confirm_password_err = $email_err = $fullName_err = "";
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+
     // Validate username
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
@@ -20,19 +20,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         // Prepare a select statement
         $sql = "SELECT username FROM users WHERE username = ?";
-        
+
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
+
             // Set parameters
             $param_username = trim($_POST["username"]);
-            
+
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 /* store result */
                 mysqli_stmt_store_result($stmt);
-                
+
                 if(mysqli_stmt_num_rows($stmt) == 1){
                     $username_err = "This username is already taken.";
                 } else{
@@ -46,19 +46,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
+
     // Validate password
     if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
+        $password_err = "Please enter a password.";
     } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
+        $password_err = "Password must have at least 6 characters.";
     } else{
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate confirm password
     if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
+        $confirm_password_err = "Please confirm password.";
     } else{
         $confirm_password = trim($_POST["confirm_password"]);
         if(empty($password_err) && ($password != $confirm_password)){
@@ -66,36 +66,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
 
-    // Validate fullname
-    if(empty(trim($_POST["fullname"]))){
-        $fullname_err = "Please confirm full name.";     
+    // Validate fullName
+    if(empty(trim($_POST["fullName"]))){
+        $fullName_err = "Please confirm full name.";
     } else{
-        $fullname = trim($_POST["fullname"]);
+        $fullName = trim($_POST["fullName"]);
     }
 
     // Validate email
-    if(empty(trim($_POST["email"]))){
-        $email_err = "Please confirm email.";     
+    if(empty(trim($_POST["userEmail"]))){
+        $email_err = "Please confirm email.";
     } else{
-        $email = trim($_POST["email"]);
+        $email = trim($_POST["userEmail"]);
     }
-    
+
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($fullname_err) && empty($email_err)){
-        
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($fullName_err) && empty($email_err)){
+
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password, fullname, email) VALUES (?, ?, ?, ?)";
-         
+        $sql = "INSERT INTO users (username, password, fullName, userEmail) VALUES (?, ?, ?, ?)";
+
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password, $param_fullname, $param_email);
-            
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password, $param_fullName, $param_email);
+
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            $param_fullname = $fullname;
-            $param_email = $email;
-            
+            $param_fullName = $fullName;
+            $param_email = $userEmail;
+
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
@@ -108,57 +108,62 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
+
     // Close connection
     mysqli_close($link);
 }
 ?>
- 
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Full Name</label>
-                <input type="text" name="fullname">
-            </div>    
-            <div class="form-group">
-                <label>email</label>
-                <input type="text" name="email">
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
-            </div>
-            <p>Already have an account? <a href="loginPage.php">Login here</a>.</p>
-        </form>
-    </div>    
-</body>
+    <html lang="en">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <head>
+        <title>Sign Up</title>
+    </head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <body>
+        <div class="signUpBox">
+            <img src="images/avatar.png" class ="avatar" alt="Avatar">
+            <h1>Sign Up Here</h1>
+            <h2>Please fill this form to create an account.</h2>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+                <p>Username</p>
+                    <label>
+                        <input type="text" name="userName" class="form-control <?php echo (!empty($userName_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>" placeholder="Create Your Username">
+                        <span class="invalid-feedback"><?php echo $userName_err; ?></span>
+                    </label>
+
+                <p>Password</p>
+                    <label>
+                        <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" placeholder="Enter Password">
+                        <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                    </label>
+
+                <p>Confirm Password</p>
+                    <label>
+                        <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>" placeholder="Re-Enter Password">
+                        <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                    </label>
+
+                <p>Full Name </p>
+                    <label>
+                        <input type="text" name="fullName" class="form-control <?php echo (!empty($fullName_err)) ? 'is-invalid': ''; ?>" value="<?php echo $fullName; ?>" placeholder="Enter Your Full Name">
+                        <span class="invalid-feedback"><?php echo $fullName_err; ?></span>
+                    </label>
+
+                <p>Email</p>
+                    <label>
+                        <input type="text" name="userEmail" class="form-control <?php echo(!empty($userEmail_err)) ? 'is-invalid': ''; ?>" value="<?php echo $userEmail; ?>" placeholder="Enter Your Email">
+                        <span class="invalid-feedback"><?php echo $userEmail_err; ?></span>
+                    </label>
+
+                <input type="submit" name="" value="Submit"><br/>
+                <a href="loginPage.php">Already have an account? Login here.</a>
+            </form>
+        </div>
+    </body>
 </html>
