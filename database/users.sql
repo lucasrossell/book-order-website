966d22648865771af49adde1e06764c9e3d7ea23
Luis Vargas <32406288+lvarg046@users.noreply.github.com>
2021-11-29 21:59:43 -0500

    changed table variable names to work with register.php

--- database/users.sql
+++ database/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(24) NOT NULL UNIQUE,
   `password` varchar(50) NOT NULL,
-  `fullname` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `fullName` varchar(100) NOT NULL,
+  `userEmail` varchar(50) NOT NULL,
   PRIMARY KEY (`username`)
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8;
