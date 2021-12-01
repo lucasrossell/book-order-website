13fe926eb32d0cf59e64cdbb9cadf199612bae20
Luis Vargas <32406288+lvarg046@users.noreply.github.com>
2021-11-30 23:35:24 -0500

    Creating the book request form frontend

---
+++ bookRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<meta name="bookReq" content="width=device-width,initial-scale=1">
+    <head>
+        <title>
+            Book Request Form
+        </title>
+    </head>
+<link rel="stylesheet" type="text/css" href="style.css">
+
