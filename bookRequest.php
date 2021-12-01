f37bf6055a6ed8745c653dbc7afe7f9eab80e5f3
Shivani Kumar <shivanikumar1920@yahoo.com>
2021-12-01 10:27:16 -0500

    Updating frontend to match backend

--- bookRequest.php
+++ bookRequest.php
@@ -73,17 +73,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             mysqli_stmt_close($stmt);
         }
     }
+    // Close connection
+    mysqli_close($link);
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<meta charset="utf-8">
-<meta name="bookReq" content="width=device-width,initial-scale=1">
+    <html lang="en">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
     <head>
-        <title>
-            Book Request Form
-        </title>
+        <title>Book Request Form</title>
     </head>
-<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <body>
+        <div class="bookReqForm">
+            <!--<img src="images/avatar.png" class ="avatar" alt="Avatar"> -->
+            <h1>Book Request Form</h1>
+            <h2>Enter the book's information that you would like to request.</h2><br/>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+                <p>Title</p>
+                    <label>
+                        <input type="text" name="title" class="form-control <?php echo (!empty($title_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $title; ?>" placeholder="Enter the Book's Title">
+                        <span class="invalid-feedback"><?php echo $title_err; ?></span>
+                    </label>
+
+                <p>Author</p>
+                    <label>
+                        <input type="text" name="author" class="form-control <?php echo (!empty($author_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $author; ?>" placeholder="Enter Author's Name">
+                        <span class="invalid-feedback"><?php echo $author_err; ?></span>
+                    </label>
+
+                <p>Edition</p>
+                    <label>
+                        <input type="text" name="edition" class="form-control <?php echo (!empty($edition_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $edition; ?>" placeholder="Enter tbe Book's Edition">
+                        <span class="invalid-feedback"><?php echo $edition_err; ?></span>
+                    </label>
+
+                <p>Publisher </p>
+                    <label>
+                        <input type="text" name="publisher" class="form-control <?php echo (!empty($publisher_err)) ? 'is-invalid': ''; ?>" value="<?php echo $publisher; ?>" placeholder="Enter the Publisher">
+                        <span class="invalid-feedback"><?php echo $publisher_err; ?></span>
+                    </label>
+
+                <p>ISBN</p>
+                    <label>
+                        <input type="text" name="ISBN" class="form-control <?php echo(!empty($ISBN_err)) ? 'is-invalid': ''; ?>" value="<?php echo $ISBN; ?>" placeholder="Enter the ISBN">
+                        <span class="invalid-feedback"><?php echo $ISBN_err; ?></span>
+                    </label>
+
+                <input type="submit" name="" value="Submit"><br/>
+            </form>
+        </div>
+    </body>
+</html>
 
