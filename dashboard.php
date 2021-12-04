bd177e1e4ccf73bdbb8b02355dab018e066f686c
Luis Vargas <32406288+lvarg046@users.noreply.github.com>
2021-12-04 13:13:17 -0500

    Dashboard.php creation & CSS addition

---
+++ dashboard.php
@@ -0,0 +1,30 @@
+<?php
+//    require_once "register.php";
+//?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Dashboard View</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body id="dash-body">
+        <div class="dash-header">
+            <h1>Dashboard</h1>
+        </div>
+        <div class="dash-row">
+            <div class="dash-col-3 dash-col-s-3 dash-menu">
+                <ul>
+                    <li>Create New Book Order</li>
+                    <li>View/Edit Existing Order Form</li>
+                    <li>Change Password</li>
+                    <li>Log-Out</li>
+                </ul>
+            </div>
+        </div>
+
+    </body>
+
+</html>
