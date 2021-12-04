32911c88967ba62f18c33ffb05dc9cc6e5f4eb4a
Luis Vargas <32406288+lvarg046@users.noreply.github.com>
2021-12-04 16:49:58 -0500

    admin dashboard.php update

--- dashboard.php
+++ dashboard.php
@@ -1,30 +1,44 @@
 <?php
-//    require_once "register.php";
+//    PHP stuff goes here
 //?>
+
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
         <title>Dashboard View</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <body id="dash-body">
-        <div class="dash-header">
-            <h1>Dashboard</h1>
-        </div>
-        <div class="dash-row">
-            <div class="dash-col-3 dash-col-s-3 dash-menu">
-                <ul>
-                    <li>Create New Book Order</li>
-                    <li>View/Edit Existing Order Form</li>
-                    <li>Change Password</li>
-                    <li>Log-Out</li>
-                </ul>
-            </div>
+    <body id="dash">
+        <div id="sidebar-nav" class="sidebar-nav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a class="active-nav" href="dashboard.php">Home</a>
+            <a href="bookRequest.php">New Book Order </a> <!-- redirects to create a new request -->
+            <a href="usersList.php">View All Users</a> <!-- When clicked shows the form on Dashboard Page in aside below-->
+            <a href="#">Change Password</a><!-- TODO: Create Change Password Screen-->
+            <a href="#">Log-Out</a><!-- TODO: Create Log-Out Screen-->
         </div>
 
+    <div id="main" class="dash-content">
+        <span id="menu-icon" style="font-size:30px;cursor:pointer position:absolute;" onclick="openNav()">&#9776; Dashboard</span>
+<!--        <h2>Dashboard content??</h2>-->
+<!--        TODO: Add PHP to display Open Orders here -->
+
+    </div>
+        <script>
+            function openNav() {
+                document.getElementById("sidebar-nav").style.width = "250px";
+                document.getElementById("menu-icon").style.display = "none";
+                document.getElementById("main").style.marginLeft = "250px";
+            }
+
+            function closeNav() {
+                document.getElementById("sidebar-nav").style.width = "0";
+                document.getElementById("menu-icon").style.display = "";
+                document.getElementById("main").style.marginLeft= "0";
+            }
+        </script>
     </body>
-
 </html>
