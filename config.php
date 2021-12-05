2e964bd2cfa1021beb00aae6f6da55786cc14d4c
Luis Vargas <32406288+lvarg046@users.noreply.github.com>
2021-12-05 18:45:27 -0500

    config update to return the $link to the database

--- config.php
+++ config.php
@@ -7,10 +7,12 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'bookOrderWebsite');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_NAME);
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+return $link;
 ?>
