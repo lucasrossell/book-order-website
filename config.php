7e88e349b13abf2e937e0433c7090d540ab5a046
Shivani Kumar <shivanikumar1920@yahoo.com>
2021-12-06 13:42:37 -0500

    config fix

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_NAME);
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect." . mysqli_connect_error());
 }
 
 return $link;
