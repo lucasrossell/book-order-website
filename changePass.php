7e1f40f93f0d8974040bdb2bfadcc1ed40a599b5
Shivani Kumar <shivanik19@knights.ucf.edu>
2021-12-06 20:57:41 -0500

    adding frontend

--- changePass.php
+++ changePass.php
@@ -12,3 +12,22 @@ if($_POST["newPassword"] == $row["confirmPassword"]) {
     $message = "Password is not correct";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Change Password</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div><?php if(isset($message)) { echo $message; } ?></div>
+        <form method="POST">
+            <p>New Password:</p>
+            <input type="text" name="newPassword"><span id="newPassword" class="required"></span>
+            <p>Confirm Password:</p>
+            <input type="text" name="confirmPassword"><span id="confirmPassword" class="required"></span>
+            <input type="submit" name="update" value="Update">
+        </form>
+    </body>
+</html>
