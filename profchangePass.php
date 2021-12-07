2fd35d0bed506c59faa2221f8bb38040bf5a5d30
William Cross <WilliamCross.edu@gmail.com>
2021-12-07 15:52:06 -0500

    css profChangePass

--- profchangePass.php
+++ profchangePass.php
@@ -36,8 +36,10 @@ if(isset($_POST['submit'])) {
 <html lang="en">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <head></head>
-    <body>
+    <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body class="profChange">
         <h1>Change Password</h1>
         <form action = '' method="POST">
             <p>Username:</p>
