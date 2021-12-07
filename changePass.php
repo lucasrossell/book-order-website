5d3309be0eda2d17cd2dbb659a341b47cd6deeb6
William Cross <WilliamCross.edu@gmail.com>
2021-12-07 14:39:47 -0500

    added css to changePass

--- changePass.php
+++ changePass.php
@@ -43,8 +43,10 @@ if(isset($_POST['submit'])) {
 <html lang="en">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <head></head>
-    <body>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body class="changePass">
         <h1>Change Password</h1>
         <form action = '' method="POST">
             <p>Username:</p>
