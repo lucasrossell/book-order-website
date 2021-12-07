ebafcd62a0bd2457b76a39e155ca18673f8d273d
Shivani Kumar <shivanikumar1920@yahoo.com>
2021-12-07 12:48:12 -0500

    fix pass

--- resetPass.php
+++ resetPass.php
@@ -2,26 +2,25 @@
 require_once "config.php";
 
 if(isset($_POST['submit'])) {
-    $body = $_POST['body'];
-    $subject = $_POST['subject'];
+    $username = $_POST['username'];
+    $sql= "SELECT username, userEmail FROM users where username = '$username'";
     $headers = "From: bookstore@ucf.edu";
     $subject = "Temporary Password - UCF Books";
+    $tempPass = substr(md5(uniqid(rand(),1)),3,10);
+    $edit = mysqli_query($link, "update users set password = '$tempPass' where username='$username'");
     $body = "Your temporary password is : $tempPass. If you did not request this temporary password email, you may want to log in and change your password for security reasons.";
-
     $data = mysqli_query($link, $sql);
     $row = mysqli_fetch_assoc($data);
-    $fetch_user_id=$row['username'];
-    if ($recipient == $fetch_user_id) {
+    $fetch_user_id = $row['username'];
+    if ($username == $fetch_user_id) {
         $email=$row['userEmail'];
         $to = $email;
         mail($to,$subject,$body,$headers);
-        echo "Mail sent."; exit;
-    }
-    else {
+        header("location:loginPage.php"); exit;
+    } else {
         echo "Invalid username.";
     }
 }
-header("location:loginPage.php"); exit;
 ?>
 
 <!DOCTYPE html>
