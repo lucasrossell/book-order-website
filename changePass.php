0ba29c8870d92c1b07ead0157f07005aea097506
Luis Vargas <32406288+lvarg046@users.noreply.github.com>
2021-12-07 01:21:03 -0500

    using the correct database variable $link not $db

--- changePass.php
+++ changePass.php
@@ -3,10 +3,10 @@
 require_once "config.php";
 // Getting the password of the user from database and changing it.
 $username = $_GET['username'];
-$result = mysqli_query($db, "select * from users where username='$username'");
+$result = mysqli_query($link, "select * from users where username='$username'");
 $row = mysqli_fetch_array($result);
 if($_POST["newPassword"] == $row["confirmPassword"]) {
-    mysqli_query($db, "update users set password ='" . $_POST["newPassword"] . "' where username='$username'");
+    mysqli_query($link, "update users set password ='" . $_POST["newPassword"] . "' where username='$username'");
     $message = "Password Changed Successfully";
     header("location: adminDash.php"); exit;
 } else {
--- deleteBookReq.php
+++ deleteBookReq.php
@@ -3,10 +3,10 @@
 require_once "config.php";
 
 $order_id = $_GET['order_id'];
-$del = mysqli_query($db, "delete from books where order_id='$order_id");
+$del = mysqli_query($link, "delete from books where order_id='$order_id");
 
 if($del) {
-    mysqli_close($db);
+    mysqli_close($link);
     header("location:openOrder.php");
     exit;
 } else {
