1bdaef472f92c373451c5754429c3539122e39bf
Shivani Kumar <shivanikumar1920@yahoo.com>
2021-12-06 23:34:06 -0500

    phpfix

--- editUsers.php
+++ editUsers.php
@@ -9,7 +9,7 @@ if(isset($_POST['update'])) {
     $fullName = $_POST['fullName'];
     $userEmail = $_POST['userEmail'];
     $sql = "update users set fullName = '$fullName', userEmail='$userEmail' where username = '$username'";
-
+    $data = mysqli_query($link, $sql);
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
         if(mysqli_stmt_execute($stmt)){
